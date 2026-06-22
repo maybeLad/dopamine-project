1e0b354b397e1997fba054b36d710730bc66c32e
maybeLad <okarmaagaming@gmail.com>
2026-06-22 13:55:03 +0200

    Map all servlet in web.xml file

--- src/main/java/it/dopamine/controller/SignupServlet.java
+++ src/main/java/it/dopamine/controller/SignupServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // Servlet per richiedere la pagina "signup.jsp"
-@WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
