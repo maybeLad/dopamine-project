081df11c163d09e29b764812418c800d47031886
unknown <Ciccio4913>
2026-07-20 19:08:19 +0200

    Modificato script removeCartItems.js

--- WebContent/scripts/removeCartItems.js
+++ WebContent/scripts/removeCartItems.js
@@ -1,24 +1,9 @@
-function addToCart(idProdotto) {
-    const quantityInput = document.getElementById('quantity');
-    const quantity = quantityInput ? quantityInput.value : 1;
-
-    fetch("/cart?action=add&id=" + idProdotto + "&qty=" + quantity, { 
-        method: "POST" 
-    })
-    .then(res => {
-        if (res.ok) {
-            alert("Prodotto aggiunto al carrello con successo!");
-        } else {
-            res.text().then(text => alert("Errore: " + text));
-        }
-    })
-    .catch(err => console.error("Errore nell'invio:", err));
-}
-
 function removeFromCart(idProdotto) {
     if (!confirm("Vuoi davvero rimuovere questo prodotto dal carrello?")) return;
 
-    fetch("/cart?action=remove&id=" + idProdotto, { 
+    const contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
+
+    fetch(contextPath + "/cart?action=remove&id=" + idProdotto, { 
         method: "POST" 
     })
     .then(res => {
@@ -27,7 +12,6 @@ function removeFromCart(idProdotto) {
             if (rigaProdotto) {
                 rigaProdotto.remove();
             }
-            
             return res.text();
         } else {
             throw new Error("Impossibile rimuovere il prodotto.");
