0ff393235fffa13bac747317d3df98b0428cc0c1
Ciccio4913 <francescopesce2017@gmail.com>
2026-07-21 18:04:01 +0200

    Modificato script increaseQuantity.js

--- WebContent/scripts/increaseQuantity.js
+++ WebContent/scripts/increaseQuantity.js
@@ -14,25 +14,3 @@ function decreaseQty() {
         input.value = value - 1;
     }
 }
-
-function addToCart(idProdotto) {
-    const quantityInput = document.getElementById('quantity');
-    const quantity = quantityInput ? quantityInput.value : 1;
-
-    const contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
-
-    fetch(contextPath + "/cart?action=add&id=" + idProdotto + "&qty=" + quantity, { 
-        method: "POST" 
-    })
-    .then(res => {
-        if (res.ok) {
-            alert("Prodotto aggiunto al carrello con successo!");
-        } else {
-            res.text().then(text => alert("Errore: " + text));
-        }
-    })
-    .catch(err => {
-        console.error("Errore nell'invio della richiesta:", err);
-        alert("Si è verificato un errore di rete. Riprova più tardi.");
-    });
-}
