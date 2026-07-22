661af91e4f452e09c6d77d84504f90d3938cc975
maybeLad <okarmaagaming@gmail.com>
2026-07-22 16:19:37 +0200

    Add data source with listener

--- src/main/java/it/dopamine/controller/CatalogueServlet.java
+++ src/main/java/it/dopamine/controller/CatalogueServlet.java
@@ -14,8 +14,7 @@ import it.dopamine.model.Prodotto;
 public class CatalogueServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		Map<String, List<Prodotto>> catalogo = (new ProdottoDAO()).getProdottiRaggruppati();
 		request.setAttribute("catalogo", catalogo);
