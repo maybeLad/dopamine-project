74a09d4d893b3ed13e09122d8d2fd12e0b22c25c
Ciccio4913 <francescopesce2017@gmail.com>
2026-07-22 16:50:12 +0200

    Modificato DAO UtenteDAO.java

--- src/main/java/it/dopamine/dao/UtenteDAO.java
+++ src/main/java/it/dopamine/dao/UtenteDAO.java
@@ -12,6 +12,7 @@ import java.sql.Statement;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class UtenteDAO {
+
     public int registraUtente(Utente utente) {
         String INSERT_USER_SQL = "INSERT INTO utenti" +
                 " (admin, nome, cognome, indirizzo, telefono, email, password, metodo_pagamento, carta_ultime_4_cifre, carta_scadenza) VALUES " +
@@ -20,8 +21,8 @@ public class UtenteDAO {
         int ris = 0;
         
         try (Connection connessione = Connector.getConnection();
-        		PreparedStatement ps = connessione.prepareStatement(INSERT_USER_SQL, Statement.RETURN_GENERATED_KEYS)
-        	) {
+             PreparedStatement ps = connessione.prepareStatement(INSERT_USER_SQL, Statement.RETURN_GENERATED_KEYS)
+            ) {
 
             ps.setBoolean(1, utente.isAdmin());
             ps.setString(2, utente.getNome());
@@ -40,81 +41,81 @@ public class UtenteDAO {
             e.printStackTrace();
         }
 
-
         return ris;
     }
     
     public Utente getUtente(int id) {
-    	Utente u = null;
-    	
-    	String SEARCH_USER = "SELECT * FROM utenti WHERE id_utente = ?";
+        Utente u = null;
+        String SEARCH_USER = "SELECT * FROM utenti WHERE id_utente = ?";
         
-    	try (Connection connessione = Connector.getConnection();
-                PreparedStatement ps = connessione.prepareStatement(SEARCH_USER, Statement.RETURN_GENERATED_KEYS)) {
+        try (Connection connessione = Connector.getConnection();
+             PreparedStatement ps = connessione.prepareStatement(SEARCH_USER)) {
 
             ps.setInt(1, id);
 
             try(ResultSet rs = ps.executeQuery()){
-            	if(rs.next()) {
-            		u = new Utente();
-            		
-            		u.setId(id);
-            		u.setNome(rs.getString("nome"));
-            		u.setCognome(rs.getString("cognome"));
-            		u.setEmail(rs.getString("email"));
-            		u.setIndirizzo(rs.getString("indirizzo"));
-            		u.setTelefono(rs.getString("telefono"));
-            		u.setPassword(rs.getString("password"));
-            		u.setMetodo_pagamento(rs.getString("metodo_pagamento"));
-            		u.setCarta_ultime_4_cifre(rs.getString("carta_ultime_4_cifre"));
-            		u.setScadenza_carta(rs.getString("carta_scadenza"));
-            	}
+                if(rs.next()) {
+                    u = new Utente();
+                    
+                    u.setId(id);
+                    if (rs.getBoolean("admin")) {
+                        u.setAdmin();
+                    }
+                    u.setNome(rs.getString("nome"));
+                    u.setCognome(rs.getString("cognome"));
+                    u.setEmail(rs.getString("email"));
+                    u.setIndirizzo(rs.getString("indirizzo"));
+                    u.setTelefono(rs.getString("telefono"));
+                    u.setPassword(rs.getString("password"));
+                    u.setMetodo_pagamento(rs.getString("metodo_pagamento"));
+                    u.setCarta_ultime_4_cifre(rs.getString("carta_ultime_4_cifre"));
+                    u.setScadenza_carta(rs.getString("carta_scadenza"));
+                }
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-
         return u;
     } 
     
     public Utente checkLogin(String email, String password) {
-    	String CHECK_USER = "SELECT * FROM utenti WHERE email = ?";
-    	Utente u = null;
-    	
-    	try(Connection connessione = Connector.getConnection();
-    			PreparedStatement ps = connessione.prepareStatement(CHECK_USER)){
-    		
-    		ps.setString(1, email);
-    		
-    		try(ResultSet rs = ps.executeQuery()){
-    			if(rs.next()) {
-    				
-    				String hashPassword = rs.getString("password");
-    				if(BCrypt.checkpw(password, hashPassword)) {
-	    				u = new Utente();
-	    				
-	    				u.setId(rs.getInt("id_utente"));
-	            		u.setNome(rs.getString("nome"));
-	            		u.setCognome(rs.getString("cognome"));
-	            		u.setEmail(email);
-	            		u.setIndirizzo(rs.getString("indirizzo"));
-	            		u.setTelefono(rs.getString("telefono"));
-	            		u.setMetodo_pagamento(rs.getString("metodo_pagamento"));
-	            		u.setCarta_ultime_4_cifre(rs.getString("carta_ultime_4_cifre"));
-	            		u.setScadenza_carta(rs.getString("carta_scadenza"));
-    				}
-    			}
-    		}
-    		
-    		
-    		
-    	}catch (SQLException e) {
-    		e.printStackTrace();
-    	}
-    	
-    	return u;
+        String CHECK_USER = "SELECT * FROM utenti WHERE email = ?";
+        Utente u = null;
+        
+        try(Connection connessione = Connector.getConnection();
+            PreparedStatement ps = connessione.prepareStatement(CHECK_USER)){
+            
+            ps.setString(1, email);
+            
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()) {
+                    String hashPassword = rs.getString("password");
+                    if(BCrypt.checkpw(password, hashPassword)) {
+                        u = new Utente();
+                        
+                        u.setId(rs.getInt("id_utente"));
+                        if (rs.getBoolean("admin")) {
+                            u.setAdmin();
+                        }
+                        u.setNome(rs.getString("nome"));
+                        u.setCognome(rs.getString("cognome"));
+                        u.setEmail(email);
+                        u.setIndirizzo(rs.getString("indirizzo"));
+                        u.setTelefono(rs.getString("telefono"));
+                        u.setMetodo_pagamento(rs.getString("metodo_pagamento"));
+                        u.setCarta_ultime_4_cifre(rs.getString("carta_ultime_4_cifre"));
+                        u.setScadenza_carta(rs.getString("carta_scadenza"));
+                    }
+                }
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return u;
     }
 
     public boolean cambiaPassword(String email, String oldPassword, String newPassword) {
@@ -126,7 +127,6 @@ public class UtenteDAO {
             ps.setString(1, email);
 
             try (ResultSet rs = ps.executeQuery()) {
-
                 if (rs.next()) {
                     String passwordSalvata = rs.getString("password");
 
@@ -175,27 +175,21 @@ public class UtenteDAO {
     }
 
     public boolean changePaymentMethod(String metodoPagamento, String ultimeCifreCarta, String scadenzaCarta, int id) {
-
         final String UPDATE_SQL = 
                 "UPDATE utenti SET metodo_pagamento = ?, carta_ultime_4_cifre = ?, carta_scadenza = ? WHERE id_utente = ?";
 
         try (Connection connessione = Connector.getConnection();
              PreparedStatement ps = connessione.prepareStatement(UPDATE_SQL)) {
 
-
             ps.setString(1, metodoPagamento);
             ps.setString(2, ultimeCifreCarta);
             ps.setString(3, scadenzaCarta);
             ps.setInt(4, id);
 
-
             int righeAggiornate = ps.executeUpdate();
-
             return righeAggiornate > 0;
 
-
         } catch (SQLException e) {
-
             e.printStackTrace();
             return false;
         }
